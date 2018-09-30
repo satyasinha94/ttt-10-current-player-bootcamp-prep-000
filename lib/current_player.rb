846b686e128c6ca05a0be2e888007adb77679cb5
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-30 23:51:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,13 @@
 def turn_count(board)
 turncount = 0
-  board.each do |turn|
-    if turn == "X" || turn == "O"
+  board.each do |space|
+    if space == "X" || space == "O"
       turncount += 1
     end
   end
   return turncount
+end
+
+def current_player
+
 end
